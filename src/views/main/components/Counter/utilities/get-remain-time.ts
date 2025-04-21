3f0fa58366936422f37b1e64babfc2c54c4a0baf
Elian <93670966+epaniaguam@users.noreply.github.com>
2025-04-20 22:04:20 -0500

    Feat/cambios esteticos main count (#3)
    * update: Actualización del footer con datos del evento 2025 (#1)
    
    * Cambios en titulo, agregación de contador, cambio de logo
    
    ---------
    
    Co-authored-by: Oscar Choque <89710118+oscarchoq@users.noreply.github.com>
    Co-authored-by: epaniaguam <eypaniagua@unjbg.edu.pe>

--- src/views/main/components/Counter/utilities/get-remain-time.ts
+++ src/views/main/components/Counter/utilities/get-remain-time.ts
@@ -1,32 +1,40 @@
 import type { RemainTime } from "../models/RemainTime";
 
 const getRemainTime = (eventDate: string): RemainTime => {
-    const now = new Date()
-    const eventTime = new Date(eventDate)
+  const now = new Date();
+  const eventTime = new Date(eventDate);
 
-    if (eventTime < now) {
-        return {
-            remainTime: 0,
-            remainSeconds: '00',
-            remainMinutes: '00',
-            remainHours: '00',
-            remainDays: '00'
-        }
-    }
-
-    const remainTime = eventTime.getTime() - now.getTime();
-    const remainSeconds = Math.floor((remainTime / 1000) % 60).toString().padStart(2, '0')
-    const remainMinutes = Math.floor((remainTime / 1000 / 60) % 60).toString().padStart(2, '0')
-    const remainHours = Math.floor((remainTime / 1000 / 60 / 60) % 24).toString().padStart(2, '0')
-    const remainDays = Math.floor(remainTime / (1000 * 60 * 60 * 24)).toString().padStart(2, '0')
-
+  if (eventTime < now) {
     return {
-        remainTime,
-        remainSeconds,
-        remainMinutes,
-        remainHours,
-        remainDays
-    }
-}
+      remainTime: 0,
+      remainSeconds: "00",
+      remainMinutes: "00",
+      remainHours: "00",
+      remainDays: "00",
+    };
+  }
 
-export default getRemainTime
+  const remainTime = eventTime.getTime() - now.getTime();
+  const remainSeconds = Math.floor((remainTime / 1000) % 60)
+    .toString()
+    .padStart(2, "0");
+  const remainMinutes = Math.floor((remainTime / 1000 / 60) % 60)
+    .toString()
+    .padStart(2, "0");
+  const remainHours = Math.floor((remainTime / 1000 / 60 / 60) % 24)
+    .toString()
+    .padStart(2, "0");
+  const remainDays = Math.floor(remainTime / (1000 * 60 * 60 * 24))
+    .toString()
+    .padStart(2, "0");
+
+  return {
+    remainTime,
+    remainSeconds,
+    remainMinutes,
+    remainHours,
+    remainDays,
+  };
+};
+
+export default getRemainTime;

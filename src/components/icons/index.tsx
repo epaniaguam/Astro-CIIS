75844bc3c9e1c7992e39330e08517c4ed9fb6b83
Hernan_Cham <hachambillac@unjbg.edu.pe>
2025-04-23 13:12:29 -0500

    feat: agregar componentes de iconos para las banderas de Argentina, Bolivia, Brasil, Chile, Cuba, Perú y Corea del Sur

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,17 @@
+import FlagArgentina from "./FlagArgentina.tsx";
+import FlagBolivia from "./FlagBolivia.tsx";
+import FlagBrazil from "./FlagBrazil.tsx";
+import FlagChile from "./FlagChile.tsx";
+import FlagCuba from "./FlagCuba.tsx";
+import FlagPeru from "./FlagPeru.tsx";
+import FlagSouthKorea from "./FlagSouthKorea.tsx";
+
+export {
+  FlagArgentina,
+  FlagBolivia,
+  FlagBrazil,
+  FlagChile,
+  FlagPeru,
+  FlagSouthKorea,
+  FlagCuba,
+};

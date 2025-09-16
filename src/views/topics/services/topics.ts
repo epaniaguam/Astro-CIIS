460ee3a031bf93f344e5ab51f922f161c4c7f068
Oscar Choque <89710118+oscarchoq@users.noreply.github.com>
2025-09-15 23:44:06 -0500

    update ejes tematicos (#20)
    * Update: Actualización de las temáticas
    
    ---------
    
    Co-authored-by: Josue07 <ejperazac@unjbg.edu.pe>

--- src/views/topics/services/topics.ts
+++ src/views/topics/services/topics.ts
@@ -3,38 +3,42 @@ import { Image1_1, Image2_1, Image1_2, Image2_2, Image1_3, Image2_3, Image1_4, I
 
 export const topic : Props[] = [
     {
-        name: "Gestión, Innovación y Desarrollo",
-        description: "Explora cómo las nuevas tecnologías pueden mejorar la gestión de proyectos y fomentar la innovación y el desarrollo sostenible.",
+        name: "Blockchain y Criptomonedas",
+        description: "Explora cómo Blockchain revoluciona la seguridad y transparencia, y cómo las criptomonedas están transformando las finanzas globales.",
         image1: Image1_1,
         image2: Image2_1,
     },
+    /*
     {
         name: "Ciencias de la Computación",
         description: "Descubre los fundamentos y avances en algoritmos, computación teórica y cuántica, y sistemas distribuidos.",
         image1: Image1_2,
         image2: Image2_2,
     },
+    */
     {
         name: "Inteligencia Artificial",
         description: "Conoce cómo las máquinas aprenden, entienden el lenguaje humano y actúan de manera inteligente, y los aspectos éticos relacionados.",
         image1: Image1_3,
         image2: Image2_3,
     },
+    /*
     {
         name: "Analítica de Datos",
         description: "Descubre cómo la Analítica de Datos extrae información clave a partir de datos, impulsando decisiones más informadas y estratégicas.",
         image1: Image1_4,
         image2: Image2_4,
     },
+    */
     {
-        name: "Ingeniería de Software",
+        name: "Ingeniería de Software y DevOps",
         description: "Explora cómo la Ingeniería de Software diseña y desarrolla aplicaciones eficientes, asegurando calidad y funcionalidad en sistemas complejos.",
         image1: Image1_5,
         image2: Image2_5,
     },
     {
-        name: "Telecomunicaciones",
-        description: "Aprende cómo las Telecomunicaciones conectan al mundo, permitiendo la transmisión de datos y comunicaciones a escala global.",
+        name: "Seguridad Informática",
+        description: "Aprende cómo la Seguridad Informática protege la información digital, previniendo amenazas y garantizando la confianza en entornos tecnológicos.",
         image1: Image1_6,
         image2: Image2_6,
     },
@@ -45,14 +49,14 @@ export const topic : Props[] = [
         image2: Image2_7,
     },
     {
-        name: "Internet de las Cosas",
+        name: "Tecnologías IoT",
         description: "Descubre cómo el IoT conecta objetos cotidianos a internet, optimizando procesos y fusionando lo físico con lo digital.",
         image1: Image1_8,
         image2: Image2_8,
     },
     {
-        name: "Minería de Datos",
-        description: "Aprende cómo la Minería de Datos descubre patrones ocultos para mejorar decisiones y procesos.",
+        name: "Desarrollo de Videojuegos",
+        description: "Aprende cómo el Desarrollo de Videojuegos crea mundos virtuales combinando arte y programación.",
         image1: Image1_9,
         image2: Image2_9,
     },
